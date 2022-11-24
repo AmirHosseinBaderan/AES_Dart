6c368ece147c95a2d325331e9aae0f4c6213a080
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-11-24 11:27:10 +0330

    create project

---
+++ aes/bin/aes.dart
@@ -0,0 +1,5 @@
+import 'package:aes/aes.dart' as aes;
+
+void main(List<String> arguments) {
+  print('Hello world: ${aes.calculate()}!');
+}
---
+++ aes/test/aes_test.dart
@@ -0,0 +1,8 @@
+import 'package:aes/aes.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
