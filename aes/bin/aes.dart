251f05bba0d4c1e98dc0eba322170f6fead005fd
amir hossin <65053996+Amir-Hossin-pr@users.noreply.github.com>
2022-11-24 11:49:49 +0330

    create encrypt and decrypt func

--- aes/bin/aes.dart
+++ aes/bin/aes.dart
@@ -1,5 +1,11 @@
-import 'package:aes/aes.dart' as aes;
+import 'package:aes/aes.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${aes.calculate()}!');
+  print('hello welcome to AES Encryption from dart');
+
+  var text = "amir hossein";
+  var key = "first-lastkey1-1";
+  print('Text is : [$text] and key is : [$key]');
+
+  enc(text, key);
 }
--- aes/lib/aes.dart
+++ aes/lib/aes.dart
@@ -1,3 +1,17 @@
-int calculate() {
-  return 6 * 7;
+import 'package:encrypt/encrypt.dart';
+
+void enc(String text, String keyInput) {
+  Key key = Key.fromUtf8(keyInput);
+  var encrypter = Encrypter(AES(
+    key,
+    mode: AESMode.ecb,
+    padding: 'PKCS7',
+  ));
+  IV iv = IV.fromSecureRandom(key.length);
+
+  var encrypted = encrypter.encrypt(text, iv: iv);
+  var decrypted = encrypter.decrypt(encrypted, iv: iv);
+
+  print(encrypted.base64);
+  print(decrypted);
 }
